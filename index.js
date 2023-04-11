773ebe87dcc1d2378618cb8c96324f5d3b61381c
Julie C <jucheng925@gmail.com>
2023-04-11 12:40:54 -0400

    filter fun

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driverNames, string) {
+    return driverNames.filter(match => string.toUpperCase() === match.toUpperCase())
+}
+
+function fuzzyMatch(driverNames, string) {
+    let charLength = string.length
+    return driverNames.filter(match => match.slice(0, charLength) === string)
+}
+
+function matchName(driverArray, string) {
+    return driverArray.filter(element => element.name === string)
+}
